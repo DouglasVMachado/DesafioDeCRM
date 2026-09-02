e253c612c821bd6234d06acfd6e37e45386d660f
Douglas Machado <douglasvilla23@gmail.com>
2026-09-01 22:17:16 -0300

    first commit

---
+++ src/main/java/com/desafio/crm/DesafioDeCRM/DesafioDeCrmApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.crm.DesafioDeCRM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDeCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDeCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/crm/DesafioDeCRM/DesafioDeCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.crm.DesafioDeCRM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioDeCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

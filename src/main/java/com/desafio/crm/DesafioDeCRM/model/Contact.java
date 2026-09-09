1ecb2768fdb23d3346e77b5abf3237a3ae74e57e
Douglas Machado <douglasvilla23@gmail.com>
2026-09-08 23:02:14 -0300

    Feat: Cria a classe contact

---
+++ src/main/java/com/desafio/crm/DesafioDeCRM/model/Contact.java
@@ -0,0 +1,26 @@
+package com.desafio.crm.DesafioDeCRM.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "contacts")
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String type;
+    private String amount;
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+}

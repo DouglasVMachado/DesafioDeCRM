296774500c8be1cc57eae0921fdeb9f492d6fe5e
Douglas Machado <douglasvilla23@gmail.com>
2026-09-09 22:01:45 -0300

    Feat: cria o service de client

---
+++ src/main/java/com/desafio/crm/DesafioDeCRM/service/ClientService.java
@@ -0,0 +1,31 @@
+package com.desafio.crm.DesafioDeCRM.service;
+
+import com.desafio.crm.DesafioDeCRM.model.Client;
+import com.desafio.crm.DesafioDeCRM.repository.ClientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClientService {
+
+    private final ClientRepository clientRepository;
+
+    public ClientService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    public Client create(Client client){
+        return clientRepository.save(client);
+    }
+
+    public List<Client> findAll(){
+        return clientRepository.findAll();
+    }
+
+    public Optional<Client> findById(Long id){
+        return clientRepository.findById(id);
+    }
+
+}

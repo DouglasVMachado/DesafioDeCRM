8a1c1510e795fee7ddbf1e5a2fab4f6d71239747
Douglas Machado <douglasvilla23@gmail.com>
2026-09-10 23:45:42 -0300

    Fix: Acaba de criar a classe response

--- src/main/java/com/desafio/crm/DesafioDeCRM/controller/response/ClientResponse.java
+++ src/main/java/com/desafio/crm/DesafioDeCRM/controller/response/ClientResponse.java
@@ -1,5 +1,8 @@
 package com.desafio.crm.DesafioDeCRM.controller.response;
 
+import lombok.Builder;
+
+@Builder
 public record ClientResponse(Long id,
                              String name,
                              String email) {

023b01387a08cf0be06c66c321cf99c89baeea91
Douglas Machado <douglasvilla23@gmail.com>
2026-09-11 23:49:05 -0300

    Fix: troca Id por id

--- src/main/java/com/desafio/crm/DesafioDeCRM/controller/response/ContactResponse.java
+++ src/main/java/com/desafio/crm/DesafioDeCRM/controller/response/ContactResponse.java
@@ -1,5 +1,8 @@
 package com.desafio.crm.DesafioDeCRM.controller.response;
 
+import lombok.Builder;
+
+@Builder
 public record ContactResponse(Long id,
                               String type,
                               String amount,
--- src/main/java/com/desafio/crm/DesafioDeCRM/model/Contact.java
+++ src/main/java/com/desafio/crm/DesafioDeCRM/model/Contact.java
@@ -16,7 +16,7 @@ public class Contact {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Id;
+    private Long id;
     private String type;
     private String amount;
     @ManyToOne

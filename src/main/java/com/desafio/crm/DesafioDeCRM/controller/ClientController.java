7de7a5e11690103093fad05ad882f8aa2ce747b7
Douglas Machado <douglasvilla23@gmail.com>
2026-09-11 23:12:41 -0300

    Feat: Cria a classe controller de Client

--- src/main/java/com/desafio/crm/DesafioDeCRM/controller/ClientController.java
+++ src/main/java/com/desafio/crm/DesafioDeCRM/controller/ClientController.java
@@ -3,11 +3,17 @@ package com.desafio.crm.DesafioDeCRM.controller;
 
 import com.desafio.crm.DesafioDeCRM.controller.request.ClientRequest;
 import com.desafio.crm.DesafioDeCRM.controller.response.ClientResponse;
+import com.desafio.crm.DesafioDeCRM.mapper.ClientMapper;
+import com.desafio.crm.DesafioDeCRM.model.Client;
 import com.desafio.crm.DesafioDeCRM.service.ClientService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/clients")
@@ -20,7 +26,23 @@ public class ClientController {
     }
 
     public ResponseEntity<ClientResponse> create(@RequestBody ClientRequest clientRequest){
+        Client newClient = ClientMapper.toClient(clientRequest);
+        Client savedClient = clientService.create(newClient);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 
+    public ResponseEntity<List<ClientResponse>> findAll(){
+        List<ClientResponse> clientResponses = clientService.findAll()
+                .stream()
+                .map(ClientMapper::toClientResponse)
+                .toList();
+        return ResponseEntity.ok().body(clientResponses);
+    }
+
+    public ResponseEntity<ClientResponse> findById(@PathVariable Long id){
+        return clientService.findById(id)
+                .map(client -> ResponseEntity.ok(ClientMapper.toClientResponse(client)))
+                .orElse(ResponseEntity.notFound().build());
     }
 
 }
